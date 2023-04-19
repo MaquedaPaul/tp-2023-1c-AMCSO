c961238e724afeca4675be1543498dd1b84ab723
MaquedaPaul <maquedapablodavid@gmail.com>
2023-04-19 22:29:02 +0000

    Profundizando en algoritmos.c (incompleto)

--- memory/include/algoritmos.h
+++ memory/include/algoritmos.h
@@ -2,7 +2,16 @@
 // Created by utnso on 4/7/23.
 //
 
-#ifndef TEMPLATE_ALGORITMOS_H
-#define TEMPLATE_ALGORITMOS_H
+#ifndef TPSO_ALGORITMOS_H
+#define TPSO_ALGORITMOS_H
+#include <stdint.h>
+#include <loggers_configs.h>
+#include <protocolo.h>
+#include <gestion_memoria.h>
+extern t_config_memory *cfg_memory;
 
+t_segmento* firstFit();
+t_segmento* bestFit();
+t_segmento* worstFit();
+t_segmento* buscarHuecoLibre(uint32_t tamanioSegmento);
 #endif //TEMPLATE_ALGORITMOS_H
--- memory/src/algoritmos.c
+++ memory/src/algoritmos.c
@@ -6,6 +6,31 @@
 
 
 
-void firstFit();
-void bestFit();
-void worstFit();
+t_segmento* firstFit(){
+    t_segmento* huecoLibre;
+    return huecoLibre;
+}
+t_segmento* bestFit(){
+    t_segmento* huecoLibre;
+    return huecoLibre;
+}
+t_segmento* worstFit(){
+    t_segmento* huecoLibre;
+    return huecoLibre;
+}
+
+
+t_segmento* buscarHuecoLibre(uint32_t tamanioSegmento){
+    char* algoritmoAsignacion = cfg_memory->ALGORITMO_ASIGNACION;
+    t_segmento* huecoLibre;
+    if(strcmp(algoritmoAsignacion,"FIRST") == 0){
+        huecoLibre= firstFit();
+    }
+    if(strcmp(algoritmoAsignacion,"BEST") == 0){
+        huecoLibre= bestFit();
+    }
+    if(strcmp(algoritmoAsignacion,"WORST") == 0){
+        huecoLibre= worstFit();
+    }
+    return huecoLibre;
+}
