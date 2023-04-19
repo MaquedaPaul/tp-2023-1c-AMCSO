03f77fa5bae8b914a8d211f3a5c698255f1700e8
MaquedaPaul <maquedapablodavid@gmail.com>
2023-04-19 22:30:14 +0000

    Profundizando en gestion_memoria (incompleto) Planteo inicial con funciones fundamentales y variables globales relevantes

--- memory/include/gestion_memoria.h
+++ memory/include/gestion_memoria.h
@@ -6,11 +6,37 @@
 #define MEMORY_GESTION_MEMORIA_H
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <commons/collections/list.h>
+#include <loggers_configs.h>
+
+extern t_log* trace_logger;
+extern t_log* debug_logger;
+extern t_log* info_logger;
+extern t_log* warning_logger;
+extern t_log* error_logger;
+extern t_config* file_cfg_memory;
+extern t_config_memory *cfg_memory;
+typedef struct
+{
+    int pid;
+    t_list* segmentos;
+} t_tablaSegmentos;
+
+typedef struct
+{
+    uint32_t base;
+    uint32_t limite;
+    uint8_t nro_segmento;
+} t_segmento;
 
 void escribirEnPosicion(uint32_t direccion);
 bool hayDisponibilidadDeEspacio();
 bool elEspacioSeEncuentraEnDiferentesHuecos();
-uint32_t realizarCreacionSegmento(uint32_t pid);
+uint32_t realizarCreacionSegmento(uint32_t pid, t_segmento* huecoLibre, uint32_t tamanio);
+bool removerDeHuecosLibres(t_segmento* huecoLibre);
+bool agregarAHuecosUsados(t_segmento* huecoLibre);
+t_segmento* dividirEnDos(t_segmento* huecoLibre,uint32_t tamanio);
 void realizarEliminacionSegmento(uint32_t direccion);
 uint32_t realizarCompactacion();
 void informarTablasActualizadas(uint32_t tablasActualizadas,int cliente_socket);
--- memory/src/gestion_memoria.c
+++ memory/src/gestion_memoria.c
@@ -3,6 +3,11 @@
 //
 
 #include <gestion_memoria.h>
+void* espacio_contiguo;
+t_list* tablasSegmentos;
+t_list* huecosUsados;
+t_list* huecosLibres;
+
 
 void escribirEnPosicion(uint32_t direccion){
 
@@ -15,9 +20,55 @@ bool elEspacioSeEncuentraEnDiferentesHuecos(){
     return true;
 }
 
-uint32_t realizarCreacionSegmento(uint32_t pid){
+t_tablaSegmentos* buscarEnTablasSegmentos(uint32_t unPid){
+    bool coincidenPids(t_tablaSegmentos* unaTabla){
+        return unaTabla->pid == unPid;
+    }
+    t_tablaSegmentos* tablaEncontrada = list_find(tablasSegmentos,coincidenPids);
+    return tablaEncontrada;
+}
+
+
+
+uint32_t realizarCreacionSegmento(uint32_t pid, t_segmento* huecoLibre, uint32_t tamanio){
+    t_segmento* segmentoParaAgregar;
+    t_tablaSegmentos* tablaEncontrada = buscarEnTablasSegmentos(pid);
+    if(huecoLibre->limite == tamanio){
+        removerDeHuecosLibres(huecoLibre);
+        agregarAHuecosUsados(huecoLibre);
+        segmentoParaAgregar = huecoLibre;
+    }
+    if(huecoLibre->limite < tamanio){
+        log_error(error_logger,"Ocurrio algo que no debia pasar, ayuda");
+        return -1;
+    }
+    if(huecoLibre->limite > tamanio){
+        segmentoParaAgregar= dividirEnDos(huecoLibre,tamanio);
+    }
+
+
+
+    list_add(tablaEncontrada->segmentos, segmentoParaAgregar);
     return 0;
 }
+
+
+
+bool removerDeHuecosLibres(t_segmento* huecoLibre){
+    return true;
+}
+bool agregarAHuecosUsados(t_segmento* huecoLibre){
+    return true;
+}
+t_segmento* dividirEnDos(t_segmento* huecoLibre,uint32_t tamanio){
+    t_segmento* segmento;
+    return segmento;
+}
+
+
+
+
+
 
 void realizarEliminacionSegmento(uint32_t direccion){
 
