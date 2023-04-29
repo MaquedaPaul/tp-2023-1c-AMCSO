04ff16250f215e5e18a2f0add9f5b669ef56d358
NicoleChavez <72278911+NicoleChavez@users.noreply.github.com>
2023-04-29 13:27:46 -0300

    update

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -1,21 +1,24 @@
 #include <commons/collections/list.h>
+#include <commons/log.h>
+
 #include <estructuras.h>
 #include <string.h>
 
 #include <protocolo.h>
 #include <mmu.h>
+#include <init.h>
 
+extern pcb* pcb_actual;
+extern int fd_memoria;
+extern int fd_kernel;
 
 void escribir_en_registro(char* registro, char valor[16]);
-
-void escribir_en_registro_de_4_bytes(char* registro, char valor[4]);
-
-void escribir_en_registro_de_8_bytes(char* registro, char valor[8]);
-
-void escribir_en_registro_de_16_bytes(char* registro, char valor[16]);
-
+char* leer_valor_de_registro(char* registro);
 int tamaño_a_escribir(char* tipo_registro);
+void ejecutar_lectura();
+void ejecutar_escritura();
+void terminar_ejecucion_lectura(char* leido);
+void terminar_ejecucion_escritura();
 
-int obtener_valor_en_memoria();
 
 
