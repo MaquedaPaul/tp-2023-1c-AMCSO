fe0b2fb0130b70aa5e21b225fe177dae54ed2697
MaquedaPaul <maquedapablodavid@gmail.com>
2023-07-17 07:56:45 +0000

    Añado ejecutables Nos facilitan el uso de las pruebas y se usan del siguiente modo ./ejecutable.sh PARAM1 PARAM2 donde ejecutable es el nombre del módulo, PARAM1 es el test concreto, por ejemplo "BASE" y PARAM2 depende del módulo. Para consola, PARAM2 es el número de test. Para los demás módulos es la IP con la que se gestiona el módulo ./console.sh BASE 1 ./kernel.sh BASE 0.0.0.0

---
+++ console/ejecutable/console.sh
@@ -0,0 +1,23 @@
+# Obtener la ubicación del script
+script_dir=$(dirname "$0")
+
+# Directorio relativo al script
+directorio=$(realpath "$0")
+directorioEjecutable=$(dirname "$directorio")
+directorioModulo=$(dirname "$directorioEjecutable")
+directorioTp=$(dirname "$directorioModulo")
+
+
+test="$1"
+testNro="$2"
+
+directorioConfig="$directorioTp/pruebas/configs/$test"
+directorioInstrucciones="$directorioTp/pruebas/instrucciones/$test/$1_$testNro"
+directorioBin="$directorioModulo/bin/"
+echo "$directorioInstrucciones"
+echo "$directorioConfig"
+
+# Cambiar al directorio
+cd "$directorioBin"
+# Ejecutar el programa con parámetros determinados
+./console.out "$directorioConfig/console.config" "$directorioInstrucciones"
